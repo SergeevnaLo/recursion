c9cbc219a99ba27f79416352831294eec7fb73a0
SergeevnaLo <lolita.shlyak72@mail.ru>
2023-10-07 17:25:09 +0300

    Добавьте файлы проекта.

---
+++ recursion/Programm.cs
@@ -0,0 +1,73 @@
+﻿// Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.WriteLine(count + " ");
+    }
+}
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.WriteLine(SumMN(m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+
+int mM = InputNumbers("Введите m: ");
+int nN = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(mM, nN);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int mM, int nN)
+{
+    if (mM == 0) return nN + 1;
+    else if (nN == 0) return Ack(mM - 1, 1);
+    else return Ack(mM - 1, Ack(mM, nN - 1));
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
